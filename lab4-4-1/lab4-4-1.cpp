4c54abd244f9dfb5d2dddf8f79344efc03068522
shengkai07115 <144308855+shengkai07115@users.noreply.github.com>
2023-09-21 10:59:26 +0800

    Add files via upload

---
+++ lab4-4-1/lab4-4-1.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <iomanip>
+using namespace std;
+
+int main(){
+	int V1;
+	long V2;
+	char V3;
+	bool V4;
+	float V5;
+	double V6;
+	
+	cout << "int(sizeof)" << sizeof (V1) << "Bytes\n";
+	cout << "long(sizeof)" << sizeof (V2) << "Bytes\n";
+	cout << "char(sizeof)" << sizeof (V3) << "Bytes\n";
+	cout << "bool(sizeof)" << sizeof (V4) << "Bytes\n";
+	cout << "float(sizeof)" << sizeof (V5) << "Bytes\n";
+	cout << "double(sizeof)" << sizeof (V6) << "Bytes\n";
+	system("Pause");
+}
